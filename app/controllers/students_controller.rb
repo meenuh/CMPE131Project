f53a93a3336cc01d1cffcac5a78be153833ed8f0
Philipp S <l4l1lu@freenet.de>
2016-03-09 20:16:39 -0800

    added profile protection. user can only edit his own profile and books.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -103,6 +103,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:name, :email, :password, :password_confirmation)
+      params.require(:student).permit(:name, :email, :password, :password_confirmation, :major)
     end
 end
